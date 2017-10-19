d453e7b8f84724dbed6f8855a1249c023861101e
max lin <lindahai0512@gmail.com>
2017-10-19 14:16:28 +0800

    improve ScreenPickerView

--- ScreenPicker/ScreenPickerView.m
+++ ScreenPicker/ScreenPickerView.m
@@ -27,17 +27,37 @@
         CGContextSetRenderingIntent(context, kCGRenderingIntentRelativeColorimetric);
         CGContextDrawImage(context, NSRectToCGRect(self.bounds), _imageRef);
     }
+
+    //Cross line
+    CGContextSetRGBStrokeColor(context, 209.0/255.0, 228.0/255.0, 173.0/255.0, 1);
+    CGFloat x = -width / 2;
+    CGFloat y = height /2;
+
+    CGContextMoveToPoint(context, x, y);
+    x = width;
+    CGContextAddLineToPoint(context, x, y);
+    CGContextStrokePath(context);
+    CGContextClosePath(context);
+    
+    x = width / 2;
+    y = -height / 2;
+    CGContextMoveToPoint(context, x, y);
+    y = height;
+    CGContextAddLineToPoint(context, x, y);
+    CGContextStrokePath(context);
+    CGContextClosePath(context);
     
     // draw the aperture
     CGFloat apertureSize = 6;
-	CGFloat x = width / 2 - apertureSize / 2;
-	CGFloat y = height / 2 - apertureSize / 2;
-	CGRect apertureRect = CGRectMake(x, y, apertureSize, apertureSize);
+    x = width / 2 - apertureSize / 2;
+    y = height / 2 - apertureSize / 2;
+    CGRect apertureRect = CGRectMake(x, y, apertureSize, apertureSize);
     CGContextSetRGBStrokeColor(context, 0, 0, 0, 1);
     CGContextSetShouldAntialias(context, NO);
     CGContextStrokeRect(context, apertureRect);
 
     // stroke outer circle
+    CGContextSetRGBStrokeColor(context, 0, 0, 0, 1);
     CGContextSetShouldAntialias(context, YES);
     CGContextSetLineWidth(context, 2.0);
     CGContextStrokeEllipseInRect(context, rect);
